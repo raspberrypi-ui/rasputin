e15aec96d48b4f7d5d3f1b0566b1ab544609b213
Simon Long <simon@raspberrypi.com>
2024-11-18 09:28:04 +0000

    Tidy wayfire

--- src/wayfire.c
+++ src/wayfire.c
@@ -30,6 +30,15 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "rasputin.h"
 
 /*----------------------------------------------------------------------------*/
+/* Typedefs and macros */
+/*----------------------------------------------------------------------------*/
+
+#define DEFAULT_KB_DELAY 400
+#define DEFAULT_KB_INTERVAL 40
+#define DEFAULT_MOUSE_SPEED 0.0
+#define DEFAULT_MOUSE_DCLICK 400
+
+/*----------------------------------------------------------------------------*/
 /* Global data */
 /*----------------------------------------------------------------------------*/
 
@@ -39,6 +48,9 @@ static GSettings *mouse_settings;
 /* Function prototypes */
 /*----------------------------------------------------------------------------*/
 
+static int read_key_file_int (GKeyFile *user, GKeyFile *sys, const char *section, const char *item, int fallback);
+static float read_key_file_float (GKeyFile *user, GKeyFile *sys, const char *section, const char *item, float fallback);
+static void write_key_file (const char *section, const char *item, int value, float fval);
 static void load_config (void);
 static void set_doubleclick (void);
 static void set_acceleration (void);
@@ -46,67 +58,91 @@ static void set_keyboard (void);
 static void set_lefthanded (void);
 
 /*----------------------------------------------------------------------------*/
+/* Helper functions */
+/*----------------------------------------------------------------------------*/
+
+static int read_key_file_int (GKeyFile *user, GKeyFile *sys, const char *section, const char *item, int fallback)
+{
+    GError *err;
+    int val;
+
+    err = NULL;
+    val = g_key_file_get_integer (user, section, item, &err);
+    if (!err && val > 0) return val;
+
+    err = NULL;
+    val = g_key_file_get_integer (sys, section, item, &err);
+    if (!err && val > 0) return val;
+
+    return fallback;
+}
+
+static float read_key_file_float (GKeyFile *user, GKeyFile *sys, const char *section, const char *item, float fallback)
+{
+    GError *err;
+    float val;
+
+    err = NULL;
+    val = g_key_file_get_double (user, section, item, &err);
+    if (!err) return val;
+
+    err = NULL;
+    val = g_key_file_get_double (sys, section, item, &err);
+    if (!err) return val;
+
+    return fallback;
+}
+
+static void write_key_file (const char *section, const char *item, int value, float fval)
+{
+    char *user_config_file, *str;
+    GKeyFile *kf;
+    gsize len;
+
+    user_config_file = g_build_filename (g_get_user_config_dir (), "wayfire.ini", NULL);
+
+    kf = g_key_file_new ();
+    g_key_file_load_from_file (kf, user_config_file, G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS, NULL);
+
+    if (value == -1) g_key_file_set_double (kf, section, item, fval);
+    else g_key_file_set_integer (kf, section, item, value);
+
+    str = g_key_file_to_data (kf, &len, NULL);
+    g_file_set_contents (user_config_file, str, len, NULL);
+    g_free (str);
+
+    g_key_file_free (kf);
+    g_free (user_config_file);
+}
+
+/*----------------------------------------------------------------------------*/
 /* Exported API */
 /*----------------------------------------------------------------------------*/
 
 static void load_config (void)
 {
-    GError *err;
     char *user_config_file;
     GKeyFile *kfu, *kfs;
 
     mouse_settings = g_settings_new ("org.gnome.desktop.peripherals.mouse");
-
     dclick = g_settings_get_int (mouse_settings, "double-click");
+    if (!dclick) dclick = DEFAULT_MOUSE_DCLICK;
 
-    /* open user and system config files */
     user_config_file = g_build_filename (g_get_user_config_dir (), "wayfire.ini", NULL);
     kfu = g_key_file_new ();
     g_key_file_load_from_file (kfu, user_config_file, G_KEY_FILE_NONE, NULL);
+    g_free (user_config_file);
 
     kfs = g_key_file_new ();
     g_key_file_load_from_file (kfs, "/etc/wayfire/defaults.ini", G_KEY_FILE_NONE, NULL);
 
-    err = NULL;
-    delay = g_key_file_get_integer (kfu, "input", "kb_repeat_delay", &err);
-    if (err)
-    {
-        err = NULL;
-        delay = g_key_file_get_integer (kfs, "input", "kb_repeat_delay", &err);
-        if (err) delay = 400;
-    }
-
-    err = NULL;
-    interval = g_key_file_get_integer (kfu, "input", "kb_repeat_rate", &err);
-    if (err)
-    {
-        err = NULL;
-        interval = g_key_file_get_integer (kfs, "input", "kb_repeat_rate", &err);
-        if (err) interval = 40;
-    }
-    interval = 1000 / interval;
-
-    err = NULL;
-    accel = g_key_file_get_double (kfu, "input", "mouse_cursor_speed", &err);
-    if (err)
-    {
-        err = NULL;
-        accel = g_key_file_get_double (kfs, "input", "mouse_cursor_speed", &err);
-        if (err) accel = 0.0;
-    }
-
-    err = NULL;
-    left_handed = g_key_file_get_boolean (kfu, "input", "left_handed_mode", &err);
-    if (err)
-    {
-        err = NULL;
-        left_handed = g_key_file_get_boolean (kfs, "input", "left_handed_mode", &err);
-        if (err) left_handed = FALSE;
-    }
+    left_handed = read_key_file_int (kfu, kfs, "input", "left_handed_mode", 0);
+    delay = read_key_file_int (kfu, kfs, "input", "kb_repeat_delay", DEFAULT_KB_DELAY);
+    interval = 1000 / read_key_file_int (kfu, kfs, "input", "kb_repeat_rate", DEFAULT_KB_INTERVAL);
+    accel = read_key_file_float (kfu, kfs, "input", "mouse_cursor_speed", DEFAULT_MOUSE_SPEED);
 
     g_key_file_free (kfu);
     g_key_file_free (kfs);
-    g_free (user_config_file);
 }
 
 static void set_doubleclick (void)
@@ -116,66 +152,18 @@ static void set_doubleclick (void)
 
 static void set_acceleration (void)
 {
-    char *user_config_file, *str;
-    GKeyFile *kf;
-    gsize len;
-
-    user_config_file = g_build_filename (g_get_user_config_dir (), "wayfire.ini", NULL);
-
-    kf = g_key_file_new ();
-    g_key_file_load_from_file (kf, user_config_file, G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS, NULL);
-
-    g_key_file_set_double (kf, "input", "mouse_cursor_speed", accel);
-
-    str = g_key_file_to_data (kf, &len, NULL);
-    g_file_set_contents (user_config_file, str, len, NULL);
-    g_free (str);
-
-    g_key_file_free (kf);
-    g_free (user_config_file);
+    write_key_file ("input", "mouse_cursor_speed", -1, accel);
 }
 
 static void set_keyboard (void)
 {
-    char *user_config_file, *str;
-    GKeyFile *kf;
-    gsize len;
-
-    user_config_file = g_build_filename (g_get_user_config_dir (), "wayfire.ini", NULL);
-
-    kf = g_key_file_new ();
-    g_key_file_load_from_file (kf, user_config_file, G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS, NULL);
-
-    g_key_file_set_integer (kf, "input", "kb_repeat_delay", delay);
-    g_key_file_set_integer (kf, "input", "kb_repeat_rate", 1000 / interval);
-
-    str = g_key_file_to_data (kf, &len, NULL);
-    g_file_set_contents (user_config_file, str, len, NULL);
-    g_free (str);
-
-    g_key_file_free (kf);
-    g_free (user_config_file);
+    write_key_file ("input", "kb_repeat_delay", delay, 0.0);
+    write_key_file ("input", "kb_repeat_rate", 1000 / interval, 0.0);
 }
 
 static void set_lefthanded (void)
 {
-    char *user_config_file, *str;
-    GKeyFile *kf;
-    gsize len;
-
-    user_config_file = g_build_filename (g_get_user_config_dir (), "wayfire.ini", NULL);
-
-    kf = g_key_file_new ();
-    g_key_file_load_from_file (kf, user_config_file, G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS, NULL);
-
-    g_key_file_set_boolean (kf, "input", "left_handed_mode", left_handed);
-
-    str = g_key_file_to_data (kf, &len, NULL);
-    g_file_set_contents (user_config_file, str, len, NULL);
-    g_free (str);
-
-    g_key_file_free (kf);
-    g_free (user_config_file);
+    write_key_file ("input", "left_handed_mode", left_handed, 0.0);
 }
 
 /*----------------------------------------------------------------------------*/
